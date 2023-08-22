6659e3ef8b876a2d4e8f9a2fb357e6d51c28476b
Ashiqul Islam Shabu <ashiq.cse3.bu@gmail.com>
2023-08-22 13:44:26 +0600

    Tech Net Cart Slice Reducer done- Redux

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import {
   Sheet,
   SheetContent,
@@ -12,15 +14,15 @@ import {
   HiOutlineTrash,
 } from 'react-icons/hi';
 import { Button } from './ui/button';
-import { IProduct } from '@/types/globalTypes';
+
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import { addToCart, removeFromCart, removeOne } from '@/redux/features/cart/cartSlice';
 
 export default function Cart() {
-  //! Dummy data
 
-  const products: IProduct[] = [];
-  const total = 0;
+const {products, total} = useAppSelector(state => state.cart)
+const dispatch = useAppDispatch()
 
-  //! **
 
   return (
     <Sheet>
@@ -47,20 +49,23 @@ export default function Cart() {
                 <h1 className="text-2xl self-center">{product?.name}</h1>
                 <p>Quantity: {product.quantity}</p>
                 <p className="text-xl">
+                 
+               
                   Total Price: {(product.price * product.quantity!).toFixed(2)}{' '}
                   $
                 </p>
               </div>
               <div className="border-l pl-5 flex flex-col justify-between">
-                <Button>
+                <Button onClick={()=> dispatch(addToCart(product))}>
                   <HiOutlinePlus size="20" />
                 </Button>
-                <Button>
+                <Button  onClick={()=> dispatch(removeOne(product))}>
                   <HiMinus size="20" />
                 </Button>
-                <Button
+                <Button onClick={()=> dispatch(removeFromCart(product))}
                   variant="destructive"
                   className="bg-red-500 hover:bg-red-400"
+                  
                 >
                   <HiOutlineTrash size="20" />
                 </Button>
---
+++ src/redux/features/products/productSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface IProduct {
+    status:boolean;
+    priceRange: number;
+}
+
+const initialState: IProduct ={
+    status: false,
+    priceRange:150,
+}
+
+
+
+const productSlice = createSlice({
+    name:"product",
+    initialState,
+    reducers: {
+
+    }
+})
+
+export default productSlice.reducer;
